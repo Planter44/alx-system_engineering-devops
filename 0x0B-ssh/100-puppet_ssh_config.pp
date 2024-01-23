df7d54a720e8b026a4e7a7da316d6853982dc54c
Planter44 <planterplanter777@gmail.com>
2024-01-23 04:17:19 -0800

    web

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Puppet to make changee in files.
+
+file { 'ect/ssh/ssh_config':
+        ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	passwordAunthentication no
+}
