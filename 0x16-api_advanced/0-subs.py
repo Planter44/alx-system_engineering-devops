817a79c1d45e62208f02207d0ded92ff84e57b91
Planter44 <planterplanter777@gmail>
2024-02-13 09:55:28 -0800

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """Retrieves the number of subscribers in a given subreddit."""
+    reddit_url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    header = {"User-Agent": "Chrome/81.0.4044.129"}
+    req = requests.get(reddit_url, headers=header)
+    reddit = req.json()
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    if (req.status_code == 200):
+        """checks if the response status is ok"""
+        subs = reddit.get("data").get("subscribers")
+        return (subs)
+    else:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
