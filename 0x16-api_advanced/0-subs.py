eb77a09798072945c447c4decc707a7c4ee1ef8e
Planter44 <planterplanter777@gmail>
2024-02-13 09:51:46 -0800

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
-    subscriber = requests.get("https://www.reddit.com/r/{}/about.json"
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if subscriber.status_code >= 300:
+    if sub_info.status_code >= 300:
         return 0
 
-    return subscriber.json().get("data").get("subscribers")
+    return sub_info.json().get("data").get("subscribers")
